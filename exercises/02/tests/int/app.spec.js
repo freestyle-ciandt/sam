b0b273d33bfe9328f7a8dd1c2d547107ea5488fb
Mateus de Almeida Pongelupe <mateusp@ciandt.com>
2021-09-29 20:13:03 -0300

    Atualizaçao exercicios e inclusao de testes para o exercicio 2

--- exercises/02/tests/int/app.spec.js
+++ exercises/02/tests/int/app.spec.js
@@ -16,5 +16,68 @@ const {
 // This is the path for the endpoint to be tested
 const API_PATH = '/exercise-02'
 
+const ID_PAYLOAD = require('./resources/id.json');
+const CITY_PAYLOAD = require('./resources/city.json');
+
 let response;
-describe(`Given the API receives a GET request on path: ${API_PATH}`, () => {});
+describe(`Given the API receives a GET request on path: ${API_PATH}/{id}`, () => {
+    const id = 1;
+    describe('When the API Key is provided', () => {
+        before(async () => {
+            response = 
+                await request(API_URL)
+                    .get(`${API_PATH}/${id}`)
+                    .set('x-api-key', API_KEY);
+        });
+
+        it('Then it should respond with 200 status code', () => {
+            expect(response.statusCode).to.eql(200);
+        });
+    
+        it('And it should respond with the expected payload', () => {
+            expect(response.body).to.eql(ID_PAYLOAD);
+        });
+    });
+
+    describe('When no API Key is provided', () => {
+        before(async () => {
+            response = 
+                await request(API_URL)
+                    .get(`${API_PATH}/${id}`);
+        });
+        it('Then it should respond with 403 status code', () => {  
+            expect(response.statusCode).to.eql(403);
+        });
+    });
+});
+
+describe(`Given the API receives a GET request on path: ${API_PATH}/cidade/{cidade}`, () => {
+    const cityName = 'Rio de Janeiro';
+    describe('When the API Key is provided', () => {
+        before(async () => {
+            response = 
+                await request(API_URL)
+                    .get(`${API_PATH}/cidade/${cityName}`)
+                    .set('x-api-key', API_KEY);
+        });
+
+        it('Then it should respond with 200 status code', () => {
+            expect(response.statusCode).to.eql(200);
+        });
+    
+        it('And it should respond with the expected payload', () => {
+            expect(response.body).to.eql(CITY_PAYLOAD);
+        });
+    });
+
+    describe('When no API Key is provided', () => {
+        before(async () => {
+            response = 
+                await request(API_URL)
+                    .get(`${API_PATH}/cidade/${cityName}`)
+        });
+        it('Then it should respond with 403 status code', () => {  
+            expect(response.statusCode).to.eql(403);
+        });
+    });
+});
