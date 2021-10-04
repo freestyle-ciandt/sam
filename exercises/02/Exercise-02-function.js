51fa422ffceb8e489e0af19ffb082a985b450925
Nevitones <neviton@ciandt.com>
2021-10-04 09:43:06 -0300

    Create new lambda function and read data from DynamoDB

--- exercises/02/Exercise-02-function.js
+++ exercises/02/Exercise-02-function.js
@@ -1,10 +1,21 @@
+const { DynamoDB } = require('aws-sdk');
+const docClient = new DynamoDB.DocumentClient();
+
 exports.handler = async function (event, context) {
-    console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-    const id = event.pathParameters.id
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: `This is the solution for exercise 02: Client ID: ${id}`,
-        }),
-      };
+	console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+	const id = parseInt(event.pathParameters.id, 10)
+
+	const params = {
+		TableName : 'mrn-clientes',
+		Key: {
+			id
+		}
+	}
+
+	const {Item:client} = await docClient.get(params).promise()
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify(client),
+	};
 };
