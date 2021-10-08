7f862ce39384ef9282bc3297f17bfa955c3ae922
Gustavo Mandolesi <mandolesi@ciandt.com>
2021-10-08 11:57:28 -0300

    Exercicio 2

--- exercises/02/Exercise-02-function.js
+++ exercises/02/Exercise-02-function.js
@@ -3,19 +3,45 @@ const docClient = new DynamoDB.DocumentClient();
 
 exports.handler = async function (event, context) {
 	console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-	const id = parseInt(event.pathParameters.id, 10)
 
+	switch(event.resource) {
+		case '/exercise-02/cidade/{cidade}': 
+			return queryByCidade(event);
+		case '/exercise-02/{id}': 
+			return queryById(event);
+	}
+};
+
+const queryById = async (event) => {
+	console.log('Query by id')
+	const id = parseInt(event.pathParameters.id, 10)
 	const params = {
 		TableName : 'mrn-clientes',
 		Key: {
 			id
 		}
 	}
-
 	const {Item:client} = await docClient.get(params).promise()
-
 	return {
 		statusCode: 200,
 		body: JSON.stringify(client),
 	};
-};
+}
+
+const queryByCidade = async (event) => {
+	console.log('Query by cidade')
+	const city = unescape(event.pathParameters.cidade)
+	const params = {
+		TableName : 'mrn-clientes',
+		IndexName : 'cidade-index',
+		KeyConditionExpression: 'cidade = :cidade',
+		ExpressionAttributeValues: {
+			':cidade': city
+		},
+	}
+	const data = await docClient.query(params).promise()
+	return {
+		statusCode: 200,
+		body: JSON.stringify(data.Items)
+	};
+}
