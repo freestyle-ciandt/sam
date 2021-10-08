df830eddda1b817f748edd41bb1fbf0e3c7e4cb6
Marco Soares <marcosoares@ciandt.com>
2021-10-08 11:17:12 -0300

    [exercise02-dma] Adding API Gateway and reading query parameters

--- exercises/02/src/index.js
+++ exercises/02/src/index.js
@@ -1,14 +1,17 @@
 const aws = require('aws-sdk')
 const docClient = new aws.DynamoDB.DocumentClient({region: process.env.AWS_REGION})
-const handler = async () => {
-    const params = {TableName: 'dma-clientes', Key: {id: 1}}
-    const result = await docClient.get(params).promise()
+const handler = async (event) => {
+    console.log('event', event);
+    const { id } = event.pathParameters
+    const params = {
+        TableName: 'dma-clientes',
+        Key: { id }
+    };
+    const { Item } = await docClient.get(params).promise();
+    console.log('Item', Item);
     return {
         statusCode: 200,
-        body: JSON.stringify({
-            message: 'it works!',
-            result
-        })
+        body: JSON.stringify(Item)
     }
 };
 
