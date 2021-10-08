1917c57ba9b92b846edebc56326d22bd6ea0df95
agathar_cit <agathar@ciandt.com>
2021-10-08 10:55:45 -0300

    pega dado de DynamoDb

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sam build --template template.yaml --build-dir .aws-sam/build &&\
-sam package --template-file .aws-sam/build/template.yaml --output-template-file .aws-sam/build/packaged-template.yaml --s3-bucket sam-ciandt-dojo --s3-prefix dma &&\
-sam deploy --template-file .aws-sam/build/packaged-template.yaml --stack-name sam-dojo-dma --no-fail-on-empty-changeset --s3-prefix dma --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM &&\
--- exercises/02/src/index.js
+++ exercises/02/src/index.js
@@ -1,9 +1,13 @@
-
+const aws = require('aws-sdk')
+const docClient = new aws.DynamoDB.DocumentClient({region: process.env.AWS_REGION})
 const handler = async () => {
+    const params = {TableName: 'dma-clientes', Key: {id: 1}}
+    const result = await docClient.get(params).promise()
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'it works!'
+            message: 'it works!',
+            result
         })
     }
 };
