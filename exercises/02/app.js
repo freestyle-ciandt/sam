29f1a3ab7c9293338052ced91a4eb75808f33e1b
agathar_cit <agathar@ciandt.com>
2021-11-29 11:34:18 -0300

    Add get handler

--- exercises/02/app.js
+++ exercises/02/app.js
@@ -1,5 +1,32 @@
-exports.handler = async () => {
-    return {
-        treta: true
+let doc = require('dynamodb-doc');
+// const AWS = require('aws-sdk');
+// const docClient = new AWS.DynamoDB.DocumentClinet();
+let dynamo = new doc.DynamoDB();
+const tableName = process.env.TableName;
+
+exports.handler = async (event) => {
+    var params = {
+        "TableName": tableName,
+        "Key": {
+            id: event.pathParameters.id
+        }
     }
+
+    docClient.get(params, function(err, data) {
+        if (err) {
+          console.log("Error", err);
+        } else {
+          console.log("Success", data.Item);
+        }
+      });
 }
+
+
+const createResponse = (statusCode, body) => {
+    return {
+        "statusCode": statusCode,
+        "body": body || ""
+    }
+};
+
+
