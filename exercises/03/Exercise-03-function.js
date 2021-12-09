ec5d279394a1192996638ef38519cc40b02ebd5d
Daniel Freitas Martins <danielm@ciandt.com>
2021-12-09 12:16:09 -0300

    Adding DynamoDB

--- exercises/03/Exercise-03-function.js
+++ exercises/03/Exercise-03-function.js
@@ -1,12 +1,14 @@
 const aws = require("aws-sdk");
+const { S3, DynamoDB } = require('aws-sdk');
 const s3 = new aws.S3();
+const docClient = new aws.DynamoDB.DocumentClient({region: 'us-east-1'});
 const csv = require("csvtojson");
 
-const { BUCKET_NAME, FILE_NAME } = process.env;
+const { BUCKET_NAME, FILE_NAME, PRODUTOS_TABLE_NAME } = process.env;
 
 exports.handler = async function (event, context) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
-
+  
   try {
     const file = await s3
       .getObject({ Bucket: BUCKET_NAME, Key: FILE_NAME })
@@ -16,6 +18,34 @@ exports.handler = async function (event, context) {
 
     const jsonArray = await csv().fromString(objectData);
     console.log(jsonArray);
+
+    const batches = [];
+    const currentBatch = [];
+    for(const item_id in jsonArray){
+      currentBatch.push({
+        PutRequest: {
+            Item: jsonArray[item_id]
+        }
+      });
+      if((parseInt(item_id)+1) % 25 == 0){
+        batches.push(currentBatch);
+        currentBatch.length = 0; // limpando o array
+      }
+    }
+
+    for(const batch_id in batches){
+      const params = {
+        RequestItems: {
+          PRODUTOS_TABLE_NAME: batches[batch_id]
+         }
+      };
+      // TODO (Unprocessed items): caso algum item falhe, você deverá tentar escrevê-lo novamente.
+      // TODO (Unprocessed items): caso algum item falhe, você deverá tentar escrevê-lo novamente.
+      // TODO (Unprocessed items): caso algum item falhe, você deverá tentar escrevê-lo novamente.
+      await docClient.batchWriteItem(params).promise();
+    }
+
+    
   } catch (err) {
     console.log(err);
   }
