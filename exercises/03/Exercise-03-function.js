52c7ed413c196946876f9adba1815f75001de640
Gustavo Mandolesi <mandolesi@ciandt.com>
2021-12-09 11:35:39 -0300

    Added new lib csvtojson to parse CSV

--- exercises/03/Exercise-03-function.js
+++ exercises/03/Exercise-03-function.js
@@ -1,16 +1,21 @@
 const aws = require("aws-sdk");
 const s3 = new aws.S3();
-const { parse } = require("csv-parse");
+const csv = require("csvtojson");
 
-const { BUCKET_NAME, FILE_NAME } = process.env
+const { BUCKET_NAME, FILE_NAME } = process.env;
 
 exports.handler = async function (event, context) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
 
   try {
-    const file = await s3.getObject({ Bucket: BUCKET_NAME, Key: FILE_NAME }).promise();
-	let objectData = file.Body.toString('utf-8');
-	console.log(objectData);
+    const file = await s3
+      .getObject({ Bucket: BUCKET_NAME, Key: FILE_NAME })
+      .promise();
+    const objectData = file.Body.toString("utf-8");
+    console.log(objectData);
+
+    const jsonArray = await csv().fromString(objectData);
+    console.log(jsonArray);
   } catch (err) {
     console.log(err);
   }
