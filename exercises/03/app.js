aab337ef611f876d320d4f4178d1fcb8f48dc8d2
Patrick Sorrentino Reis <CIANDT\patricksr@lnb021817bhz.ciandt.global>
2021-12-10 10:51:47 -0300

    handerProduto

--- exercises/03/app.js
+++ exercises/03/app.js
@@ -1,4 +1,28 @@
+const AWS = require('aws-sdk');
+AWS.config.update({ region: process.env.AWS_REGION });
+
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
+
+
+
 exports.handlerProduto = async (event) => {
+    console.log('process.env.BucketName= ', process.env.BucketName)
+
+    var bucketParams = {
+        Bucket: process.env.BucketName,
+    };
+
+
+    try {
+        const listObjects = await s3.listObjects(bucketParams).promise()
+        console.log("Success", listObjects);
+
+    } catch (error) {
+        console.log("Error", error);
+    }
+
+
     try {
         response = {
             'statusCode': 200,
