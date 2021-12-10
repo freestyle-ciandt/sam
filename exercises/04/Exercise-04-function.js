0320a851d4cd525828fc8eb16ee8976c9638d9ed
Daniel Freitas Martins <danielm@ciandt.com>
2021-12-10 11:39:28 -0300

    Adding model for cliente

--- exercises/04/Exercise-04-function.js
+++ exercises/04/Exercise-04-function.js
@@ -1,3 +1,8 @@
 exports.handler = async function (event) {
-	console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+	console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+
+	return {
+		statusCode: 200,
+		body: 'uhul'
+	};
 };
