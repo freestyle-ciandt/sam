7ad1fa1ae430fd4a918759ccbf6d386049c7cbd6
Nevitones <neviton@ciandt.com>
2021-12-10 12:11:18 -0300

    Create SQS resource and update lambda to send messages to the queue

--- exercises/04/Exercise-04-function.js
+++ exercises/04/Exercise-04-function.js
@@ -1,4 +1,26 @@
+const { SQS } = require('aws-sdk');
+const sqs = new SQS();
+const { QueueUrl } = process.env;
+
 exports.handler = async event => {
+    if (!event.body) {
+        return 
+    }
+
+    // const data = JSON.parse(event.body);
+
+    const params = {
+        MessageBody: event.body,
+        QueueUrl,
+    }
+
+    try {
+        const response = await sqs.sendMessage(params).promise()
+        console.log('sendMessage response', response);
+    } catch (err) {
+        console.log('sendMessage error', err);
+    }
+
     return createResponse(200, {
         "message": "Success"
     });
