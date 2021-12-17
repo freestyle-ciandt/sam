637f460a2db4fa9b42bc44bc7bb1c8a79df15944
Gustavo Mandolesi <mandolesi@ciandt.com>
2021-12-17 12:07:23 -0300

    Added SQS Queue to stack

--- exercises/04/Exercise-04-function.js
+++ exercises/04/Exercise-04-function.js
@@ -1,8 +1,29 @@
-exports.handler = async function (event) {
-	console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+const aws = require("aws-sdk");
 
-	return {
-		statusCode: 200,
-		body: 'uhul'
-	};
+const { QUEUE_URL } = process.env;
+
+aws.config.update({ region: 'us-east-1' });
+
+exports.handler = async function (event) {
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+
+  const { body } = event;
+
+  const sqs = new aws.SQS({ apiVersion: "2012-11-05" });
+  try {
+    const data = await sqs
+      .sendMessage({ MessageBody: JSON.stringify(body), QueueUrl: QUEUE_URL })
+      .promise();
+    console.log("Success, message sent.", data);
+    return {
+      statusCode: 200,
+      body: "Success! Item inserted in queue.",
+    };
+  } catch (err) {
+    console.log("Error", err);
+    return {
+      statusCode: 500,
+      body: "Something went wrong!",
+    };
+  }
 };
