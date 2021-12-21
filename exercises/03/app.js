1327d0ab23227bf810852e59cbe945287dd88d3e
Nevitones <neviton@ciandt.com>
2021-12-21 11:45:15 -0300

    Log S3 object format

--- exercises/03/app.js
+++ exercises/03/app.js
@@ -1,9 +1,9 @@
+import { parse } from 'csv-parse/sync';
+
 const AWS = require('aws-sdk');
 AWS.config.update({ region: process.env.AWS_REGION });
 
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
-
-
 
 
 exports.handlerProduto = async (event) => {
@@ -11,12 +11,14 @@ exports.handlerProduto = async (event) => {
 
     var bucketParams = {
         Bucket: process.env.BucketName,
+        Key: 'produtos.csv'
     };
 
 
     try {
-        const listObjects = await s3.listObjects(bucketParams).promise()
-        console.log("Success", listObjects);
+        const products = await s3.getObject(bucketParams).promise()
+        console.log("Success", products);
+        console.log("Success", products.Body);
 
     } catch (error) {
         console.log("Error", error);
