96c25ba4315c0d69d2a0e8709a6feb4b60865115
Agatha Campos de Faria Reis <CIANDT\agathar@lnb028972bhz.ciandt.global>
2021-12-21 12:15:46 -0300

    adiciona parse do csv

--- exercises/03/app.js
+++ exercises/03/app.js
@@ -1,5 +1,4 @@
-import { parse } from 'csv-parse/sync';
-
+const parse = require('csv-parse/lib/sync');
 const AWS = require('aws-sdk');
 AWS.config.update({ region: process.env.AWS_REGION });
 
@@ -15,10 +14,46 @@ exports.handlerProduto = async (event) => {
     };
 
 
+
     try {
         const products = await s3.getObject(bucketParams).promise()
-        console.log("Success", products);
-        console.log("Success", products.Body);
+        let csv = products.Body.toString('utf-8')
+
+        const csvParsed = parse(csv, {
+            columns: true,
+            skip_empty_lines: true
+          });
+        console.log('csv parsed', csvParsed)
+
+//         var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+// var params = {
+//   RequestItems: {
+//     "TABLE_NAME": [
+//        {
+//          PutRequest: {
+//            Item: {
+//              "KEY": { "N": "KEY_VALUE" },
+//                "ATTRIBUTE_1": { "S": "ATTRIBUTE_1_VALUE" },
+//                "ATTRIBUTE_2": { "N": "ATTRIBUTE_2_VALUE" }
+//            }
+//          }
+//        }
+//     ]
+//   }
+// };
+
+// ddb.batchWriteItem(params, function(err, data) {
+//   if (err) {
+//     console.log("Error", err);
+//   } else {
+//     console.log("Success", data);
+//   }
+// });
+// 1. separar itens em grupos de 25
+// 2. adequar o json ao formato do RequestItems (l. 31)
+// 3. chamar batchWriteItem
+
 
     } catch (error) {
         console.log("Error", error);
