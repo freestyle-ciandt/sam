a7f177dc06c5119a744322a49cd0880b482305b6
Daniel Freitas Martins <danielm@ciandt.com>
2021-12-21 11:43:33 -0300

    Inserting unprocessed items if there is any

--- exercises/03/Exercise-03-function.js
+++ exercises/03/Exercise-03-function.js
@@ -1,10 +1,41 @@
 const aws = require("aws-sdk");
-const { S3, DynamoDB } = require('aws-sdk');
+const { S3, DynamoDB, Inspector } = require('aws-sdk');
 const s3 = new aws.S3();
 const docClient = new aws.DynamoDB.DocumentClient({region: 'us-east-1'});
 const csv = require("csvtojson");
 
 const { BUCKET_NAME, FILE_NAME, PRODUTOS_TABLE_NAME } = process.env;
+
+const batchWrite = async (items) => {
+  const batches = [];
+    const currentBatch = [];
+    for(const item_id in items){
+      currentBatch.push({
+        PutRequest: {
+            Item: items[item_id]
+        }
+      });
+      if((parseInt(item_id)+1) % 25 == 0){
+        batches.push(currentBatch);
+        currentBatch.length = 0; // limpando o array
+      }
+    }
+
+    for(const batch_id in batches){
+      const params = {
+        RequestItems: {
+          [PRODUTOS_TABLE_NAME]: batches[batch_id]
+         }
+      };
+
+      console.log(`Calling batchWrite for batch ${batch_id}`);
+      const { UnprocessedItems } = await docClient.batchWrite(params).promise();
+      console.log(UnprocessedItems);
+      if (UnprocessedItems && UnprocessedItems.length > 0) {
+        batchWrite(UnprocessedItems);
+      }
+    }
+}
 
 exports.handler = async function (event, context) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
@@ -23,32 +54,7 @@ exports.handler = async function (event, context) {
       e['id'] = parseInt(e['id'])
     });
 
-    const batches = [];
-    const currentBatch = [];
-    for(const item_id in jsonArray){
-      currentBatch.push({
-        PutRequest: {
-            Item: jsonArray[item_id]
-        }
-      });
-      if((parseInt(item_id)+1) % 25 == 0){
-        batches.push(currentBatch);
-        currentBatch.length = 0; // limpando o array
-      }
-    }
-
-    for(const batch_id in batches){
-      const params = {
-        RequestItems: {
-          [PRODUTOS_TABLE_NAME]: batches[batch_id]
-         }
-      };
-      // TODO (Unprocessed items): caso algum item falhe, você deverá tentar escrevê-lo novamente.
-      // TODO (Unprocessed items): caso algum item falhe, você deverá tentar escrevê-lo novamente.
-      // TODO (Unprocessed items): caso algum item falhe, você deverá tentar escrevê-lo novamente.
-      await docClient.batchWrite(params).promise();
-    }
-
+    await batchWrite(jsonArray);
     
   } catch (err) {
     console.log(err);
