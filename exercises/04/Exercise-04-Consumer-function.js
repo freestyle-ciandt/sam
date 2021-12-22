b322d710b2b9bec57b7b2fbf33a61da2ca294e03
Gustavo Mandolesi <gustavo.mandolesi@gmail.com>
2021-12-22 12:01:47 -0300

    Added feature to consume messages

--- exercises/04/Exercise-04-Consumer-function.js
+++ exercises/04/Exercise-04-Consumer-function.js
@@ -1,3 +1,15 @@
 exports.handler = async function (event) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+
+  const { Records } = event;
+  
+  Records.forEach(element => {
+    const { body } = element;
+    console.log(JSON.parse(body))
+
+    // TODO NEXT
+    // Insert Records into DB
+
+    // If possible, check batch receive functionality
+  });
 };
