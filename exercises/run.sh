b62f724ed9cc4f7bc80ba6b4afa2c8f32d4402ea
Marco Soares <marcosoares@ciandt.com>
2022-01-07 12:01:26 -0300

    [daniel-marco-5] Trying to deploy the CognitoPool

--- exercises/run.sh
+++ exercises/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sam build --template template.yaml --build-dir .aws-sam/build && \
-sam package --template-file .aws-sam/build/template.yaml --output-template-file .aws-sam/build/packaged-template.yaml --s3-bucket sam-ciandt-dojo --s3-prefix os_bobos && \
-sam deploy --template-file .aws-sam/build/packaged-template.yaml --stack-name sam-dojo-os-bobos --no-fail-on-empty-changeset --s3-prefix os_bobos --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+sam package --template-file .aws-sam/build/template.yaml --output-template-file .aws-sam/build/packaged-template.yaml --s3-bucket sam-ciandt-dojo --s3-prefix daniel_marco_5 && \
+sam deploy --template-file .aws-sam/build/packaged-template.yaml --stack-name sam-dojo-daniel-marco-5 --no-fail-on-empty-changeset --s3-prefix daniel_marco_5 --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
