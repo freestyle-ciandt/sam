2417664421042172e86ca02fa10c03396d8074ef
Rômulo Tunala <romulot@ciandt.com>
2022-03-25 10:55:20 -0300

    Update exercise 03

--- exercises/03/app.js
+++ exercises/03/app.js
@@ -1,5 +1,6 @@
-const {parse} = require('csv-parse/dist/cjs/sync.cjs');
 const { DynamoDB, S3 } = require("aws-sdk");
+// const { parse } = require('csv-parse/dist/cjs/sync.cjs');
+const { parse } = require("csv-parse/sync");
 
 const { TABLE_NAME, BUCKET_NAME } = process.env;
 const docClient = new DynamoDB.DocumentClient({ region: "us-east-1" });
@@ -13,36 +14,37 @@ const params = {
 
 const getCSV = async () => {
   const produtos = await s3.getObject(params).promise();
-  return produtos.Body.toString('utf-8');
+  return produtos.Body.toString("utf-8");
 };
 const parseCsv = (csv) => {
   return parse(csv, {
     columns: true,
-    skip_empty_lines: true
-  })
+    skip_empty_lines: true,
+  });
 };
-
 const mountDynamoDBRequest = (produtos) => {
   const putRequests = produtos.map((produto) => ({ PutRequest: { Item: { ...produto } } }));
   const dynamoRequest = { RequestItems: {} };
   dynamoRequest.RequestItems[TABLE_NAME] = putRequests;
   return dynamoRequest
+};
+const writeToDatabase = async (value) => {
+  let retryCount = 0
+  const res = await docClient.batchWrite(value).promise();
+
+	if(res.UnprocessedItems && res.UnprocessedItems.length > 0) {
+		if (retryCount > 2) {
+			throw new Error(res.UnprocessedItems);
+		}
+		return batchWrite(res.UnprocessedItems, retryCount + 1);
+	}
 }
 
 exports.lambdaHandler = async () => {
-  parse()
-  
-  try {
-    response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "This is the solution for exercise 01",
-      }),
-    };
-  } catch (err) {
-    console.log(err);
-    return err;
+  const produtoResponse = await getCSV();
+  const S3Response = parseCsv(produtoResponse);
+  while(S3Response.length) {
+    const dynamoRequestItem = mountDynamoDBRequest(S3Response.splice(0,25));
+    writeToDatabase(dynamoRequestItem);
   }
-
-  return response;
 };
