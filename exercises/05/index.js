dc12bdb0e037311900288a56d63c230e9501b958
frpeixoto <fpeixoto@ciandt.com>
2022-04-01 12:21:06 -0300

    dojo 05

---
+++ exercises/05/index.js
@@ -0,0 +1,7 @@
+exports.handler = async function (event) {
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+  return {
+    statusCode: 200,
+    body: "Success! Item inserted in queue.",
+  };
+};
