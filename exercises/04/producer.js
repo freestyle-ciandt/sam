bd1f29b0670da9852544c53d7cd9da43a7daf873
Rômulo Tunala <romulot@ciandt.com>
2022-05-09 10:37:42 -0300

    WIP - Fixing the code to pass the tests

--- exercises/04/producer.js
+++ exercises/04/producer.js
@@ -2,18 +2,40 @@ const { SQS } = require("aws-sdk");
 const { SQS_URL } = process.env;
 const sqsClient = new SQS({ apiVersion: "2012-11-05" });
 
+function verifyData(body) {
+  let validate = true;
+  Object.values(body).forEach((item) => {
+    switch (typeof item) {
+      case "number":
+        if (item === 0 || !item) {
+          validate = false;
+        }
+        break;
+      case "string":
+        if (!item) {
+          validate = false;
+        }
+        break;
+    }
+  });
+  return !validate;
+}
+
 exports.post = async (event) => {
   const { body } = event;
+
   console.log("producer - body:", body);
 
-  // console.log(
+  // console.log(Object.values(body).some((item) => !item)
   //   ">> TESTE: ",
-  //   Object.values(body).some((item) => !item)
+  //   Object.values(body).some((item) => !item),
+  //   !body?.id_plano && !body?.id
   // );
-  // if (!body?.id_plano && !body?.id) {
+
+  // if (verifyData(body)) {
   //   return {
   //     statusCode: 400,
-  //     body: "Invalid Input",
+  //     body: JSON.stringify({ message: "Invalid Input" }),
   //   };
   // }
 
@@ -29,13 +51,13 @@ exports.post = async (event) => {
 
     return {
       statusCode: 200,
-      body: "Success",
+      body: JSON.stringify({ message: "Success" }),
     };
   } catch (err) {
     console.log("Error:" + JSON.stringify(err));
     return {
       statusCode: 500,
-      body: "Failed to send SQS message",
+      body: JSON.stringify({ message: "Failed to send SQS message" }),
     };
   }
 };
