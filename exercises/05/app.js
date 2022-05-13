9169adc51a60ab37002b7925f18f3d1c7502761a
Nevitones <neviton@ciandt.com>
2022-05-13 11:51:01 -0300

    Check if email is verified, add duplicated if handling, and user groups

--- exercises/05/app.js
+++ exercises/05/app.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk')
 const jose = require('jose')
 
-const { DynamoDB, CognitoIdentityServiceProvider } = AWS
+const { DynamoDB } = AWS
 const { AliasesTableName } = process.env
 
 const docClient = new DynamoDB.DocumentClient()
@@ -10,50 +10,95 @@ const getId = () => {
     return Date.now().toString(36)
 }
 
-exports.handler = async event => {
-    console.log('createAliasFunction', event)
-    
-    const { url } = JSON.parse(event.body).data
-    const idToken = event.headers.Authorization
+const saveShortUrl = async (url, uuid, groups, maxTries) => {
+    const id = getId();
+    // const id = 'l2kmkpye';
 
-    const claims = jose.decodeJwt(idToken)
-    console.log('claims', claims)
-
-    // Os dados estão sendo salvos no DB, porem não testei se está sobrescrevendo IDs existentes e o ConditionExpression: 'id <> :oldId' parece estar errado -> :newId <> :oldId
-    // O decode das informações contidas no Token está sendo feita para obter os dados do usuário
-    // Outra opção para geração de hashs:
-    //  https://stackoverflow.com/a/16110130/1723912
-    //  https://www.npmjs.com/package/hashids
-    //  var hashids = new Hashids(<url>, length:8)
-    //  console.log(hashids.encode(1))
-
-    const id = getId()
     const params = {
         TableName: AliasesTableName,
         Key: { id },
-        UpdateExpression: 'set #url=:url, #uuid=:uuid, #tipo=:tipo',
-        ConditionExpression: 'id <> :oldId',
+        UpdateExpression: 'set #prevUrl=:url, #prevUuid=:uuid, #prevTipo=:tipo',
+        ConditionExpression: '#prevId <> :id',
         ExpressionAttributeNames: {
-            '#url': 'url',
-            '#uuid': 'uuid',
-            '#tipo': 'tipo'
+            '#prevId': 'id',
+            '#prevUrl': 'url',
+            '#prevUuid': 'uuid',
+            '#prevTipo': 'tipo'
         },
         ExpressionAttributeValues: {
-            ':oldId' : id, // mudar para newId e criar um ExpressionAttributeNames para o id '#id': 'id',
+            ':id' : id, // mudar para newId e criar um ExpressionAttributeNames para o id '#id': 'id',
             ':url' : url,
-            ':uuid' : 'to-do uuid',
-            ':tipo' : 'to-do tipo'
+            ':uuid' : uuid,
+            ':tipo' : groups
         }
     }
 
     try {
-        const response = await docClient.update(params).promise()
-        console.log('DDB Update Response:', response)
+        console.log('saveShortUrl try')
+        return await docClient.update(params).promise();
     } catch (error) {
-        console.log(error)
+        console.log('saveShortUrl catch:', error)
+        if (error.code === 'ConditionalCheckFailedException') {
+            maxTries--;
+
+            if (maxTries <= 0) {
+                console.log('Max tries reached!')
+                throw error;  
+            }
+
+            console.log('Trying to save with other ID!')
+            return await saveShortUrl(url, uuid, groups, maxTries)
+        }
+    }
+}
+
+exports.handler = async event => {
+    console.log('createAliasFunction', event)
+
+    const { url } = JSON.parse(event.body).data
+    const idToken = event.headers.Authorization
+
+    const claims = jose.decodeJwt(idToken)
+    
+    
+    const {
+        email_verified: emailVerified,
+        'cognito:groups': cognitoGroups,
+        sub: uuid
+    } = claims
+
+    console.log('claims', claims)
+
+    if (!emailVerified) {
+        return {
+            body: JSON.stringify({
+                status: 'Error',
+                message: 'User not verified.'
+            })
+        }
+    }
+
+    if (!cognitoGroups || !cognitoGroups.length) {
+        return {
+            body: JSON.stringify({
+                status: 'Error',
+                message: 'Use has no group.'
+            })
+        }
+    }
+
+    const body = {};
+
+    try {
+        await saveShortUrl(url, uuid, cognitoGroups.join(), 2)
+        body.status = 'Success';
+    } catch(error) {
+        console.log('handler: saveShortUrl error', error)
+        body.status = 'Error';
+        body.error = error;
     }
 
     return {
-        body: JSON.stringify({status: 'It is alive!'})
+        body: JSON.stringify(body)
     }
 }
