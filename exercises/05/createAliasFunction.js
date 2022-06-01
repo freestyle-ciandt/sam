0dc4d2cc2d5c7d8ac242f98316d58f3b737e64b7
Rômulo Tunala <romulot@ciandt.com>
2022-06-01 11:06:32 -0300

    First step for exercise 05

--- exercises/05/createAliasFunction.js
+++ exercises/05/createAliasFunction.js
@@ -1,69 +1,78 @@
 const { DynamoDB } = require('aws-sdk');
 const jwt = require('jsonwebtoken');
 const jwkToPem = require('jwk-to-pem');
-const { TABLE_NAME } = process.env;
+const { TABLE_NAME, AWS_REGION } = process.env;
 
-const docClient = new DynamoDB.DocumentClient({ AWS_REGION });
+const docClient = new DynamoDB.DocumentClient({ region: AWS_REGION });
 
 const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const jwk = {
+  kty: 'RSA',
+  n: '4gLc5Xq9XmXz5LKjrQWkkQlDUsML_s3Uapf8LyMK7Ag3O1Wn9SOQMFn-U5kKbEunE7oga147AliVaG__FXODdcYrUM50ASYnbMPRy01BTg0RaiGv55XQajkcCX0ERVrXljwHUfqBzkaqD3usv1dGWukNNAykGRNszk9LunnUjeNDXNi1QafpYIMWXkUj_ofmSyLv6QNFgUHF9aAd7KX-Wcpgx55NBE6afG5auJlBSyrzNdBm9zK1IJ28sfFXt4gbHiIy9S6XyEb4E7xMKNOJtUIFxWcEs8yNMsi87Xeuwc8Q6yMQxstNw1hOGNVuyLpp65BhHHHz4p38b3DlzjFm6w',
+  e: 'AQAB'
+};
 
 const generateId = (length) => {
-    let result = '';
-    for (let i = 0; i< length; i++) {
-        result += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return result;
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
 }
 
 exports.lambdaHandler = async (event) => {
-    console.log('createAliasFunction - event', event);
+  // const { domainName } = event.requestContext;
+  const { url } = JSON.parse(event.body).data;
+  const idToken = event.headers.Authorization;
 
-    const { domainName } = event.requestContext;
-    const { url } = JSON.parse(event.body).data;
-    const idToken = event.headers.Authorization;
-
-    const pem = jwkToPem(jwk);
-    jwt.verify(idToken, pem, { algorithms: ['RS256'] }, function(err) {
-        if(err) {
-            return {
-                'statusCode': 403,
-                'body': JSON.stringify({
-                    message: 'Unauthorized'
-                })
-            }
-        }
-    });
-
-    const { email_verified, 'cognito:group': cognitoGroups, sub } = jwt.decode(idToken);
-
-    if(!email_verified) {
-        return {
-            'statusCode': 403,
-            'body': JSON.stringify({
-                message: 'Email is not verified'
-            })
-        }
+  const pem = jwkToPem(jwk);
+  jwt.verify(idToken, pem, { algorithms: ['RS256'] }, function (err) {
+    if (err) {
+      return {
+        'statusCode': 403,
+        'body': JSON.stringify({
+          message: 'Unauthorized'
+        })
+      }
     }
+  });
 
-    if(!cognitoGroups || !cognitoGroups.length) {
-        return {
-            'statusCode': 403,
-            'body': JSON.stringify({
-                message: 'User has no group assigned'
-            })
-        }
+  const { email_verified, 'cognito:groups': cognitoGroups, sub } = jwt.decode(idToken.replace('Bearer ', ''));
+  if (!email_verified) {
+    return {
+      'statusCode': 403,
+      'body': JSON.stringify({
+        message: 'Email is not verified'
+      })
     }
+  }
 
-    const shortenedUrlId = generateId(8);
+  if (!cognitoGroups || !cognitoGroups.length) {
+    return {
+      'statusCode': 403,
+      'body': JSON.stringify({
+        message: 'User has no group assigned'
+      })
+    }
+  }
 
-    await docClient.put({
-        TableName: TABLE_NAME,
-        Key: {
-            id: shortenedUrlId,
-            url: url,
-            uuid: sub,
-            tipo: cognitoGroups[0],
-        },
-        ConditionExpression: 'attribute_not_exists(id)',
+  const shortenedUrlId = generateId(8);
+
+  await docClient.put({
+    TableName: TABLE_NAME,
+    Item: {
+      id: shortenedUrlId,
+      url: url,
+      uuid: sub,
+      tipo: cognitoGroups[0],
+    },
+    ConditionExpression: 'attribute_not_exists(id)',
+  }).promise();
+
+  return {
+    'statusCode': 200,
+    'body': JSON.stringify({
+      message: 'Success'
     })
+  }
 }
